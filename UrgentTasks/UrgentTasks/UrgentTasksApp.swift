805f4746671771382a77b609779b04977f5f1628
Guilherme Carasek <guilhermemcarasek@outlook.com>
2025-03-25 09:46:35 +0000

    initial commit

---
+++ UrgentTasks/UrgentTasks/UrgentTasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  UrgentTasksApp.swift
+//  UrgentTasks
+//
+//  Created by MultiLab PRT 15 on 25/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct UrgentTasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
